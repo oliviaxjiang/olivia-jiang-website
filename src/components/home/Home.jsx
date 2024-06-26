94635e00961ae2e02b73fc675d6a8bc8e0c917c1
Olivia Jiang <jiangxolivia@gmail.com>
2024-06-25 17:35:24 -0700

    animated home

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,26 +1,30 @@
-import React from 'react'
-import "./home.css"
-// import Me from "../../assets"
-//46:22
-import HeaderSocials from './HeaderSocials'
-import ScrollDown from './ScrollDown'
+import React, { useEffect, useState } from 'react';
+import './home.css';
+import HeaderSocials from './HeaderSocials';
+import ScrollDown from './ScrollDown';
 
 const Home = () => {
-    return (
-        <section className="home container" id="home">
-            <div className="intro">
-                {/* <img src="" alt="" /> */}
-                <h1 className="home_name">Olivia Jiang</h1>
-                <span className="home_education">Computer Science Student @ Cornell
-                </span>
-                <HeaderSocials/>
+  const [showIntro, setShowIntro] = useState(false);
 
-                <a href="#contact" className="btn">
-                    Contact Me!
-                </a>
-                <ScrollDown/>
-            </div>
-        </section>
-    )
-}
-export default Home
+  useEffect(() => {
+    setShowIntro(true);
+  }, []);
+
+  return (
+    <section className="home container" id="home">
+      <div className={`intro ${showIntro ? 'show' : ''}`}>
+        <h1 className="home_name">Olivia Jiang</h1>
+        <span className="home_education">Computer Science Student @ Cornell</span>
+        <div className="home_rest">
+            <HeaderSocials />
+            <a href="#contact" className="btn">
+            Contact Me!
+            </a>
+            <ScrollDown />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
